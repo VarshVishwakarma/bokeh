9228494afbec980ed7507d49556ffee9f98d5c67
Mateusz Paprocki <mattpap@gmail.com>
2023-11-20 20:06:04 +0100

    Redesign and improve slider widgets

--- src/bokeh/models/widgets/sliders.pyi
+++ src/bokeh/models/widgets/sliders.pyi
@@ -14,7 +14,7 @@ if TYPE_CHECKING:
     from typing_extensions import Unpack
 
 # Bokeh imports
-from ..._types import Color, Datetime
+from ..._types import Datetime
 from ..formatters import TickFormatter
 from .widget import Widget, _WidgetInit
 
@@ -24,7 +24,7 @@ class _AbstractSliderInit(_WidgetInit, total=False):
     show_value: bool
     direction: Literal["ltr", "rtl"]
     tooltips: bool
-    bar_color: Color
+    appearance: Literal["normal", "stealth"]
 
 class AbstractSlider(Widget):
     @abstractmethod
@@ -35,7 +35,7 @@ class AbstractSlider(Widget):
     show_value: bool = ...
     direction: Literal["ltr", "rtl"] = ...
     tooltips: bool = ...
-    bar_color: Color = ...
+    appearance: Literal["normal", "stealth"] = ...
 
 class _NumericalSliderInit(_AbstractSliderInit, total=False):
     format: str | TickFormatter
